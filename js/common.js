afd02a78fac138498f86db0593d300c2a7247e38
ITkah <ilatkacenko132@gmail.com>
2020-07-02 16:51:39 +0300

    2 page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,10 @@
 $('.phone').on('input', function() {
     $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
 });
+
+$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+});
+  
