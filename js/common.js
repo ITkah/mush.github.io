531bf3a1995d124672f3a540e71c5d7a51187f74
ITkah <ilatkacenko132@gmail.com>
2020-07-03 21:28:12 +0300

    90%

--- js/common.js
+++ js/common.js
@@ -1,10 +1,15 @@
-$('.phone').on('input', function() {
-    $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
-});
+$(document).ready(function() {
 
-$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-    $(this)
-      .addClass('active').siblings().removeClass('active')
-      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    $('.phone').on('input', function() {
+        $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
+    });
+
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this).addClass('active').siblings().removeClass('active').closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+    });
+
+    $(".smm_img").on("click", function(){
+        $(this).attr("controls","")[0].play();
+    });
+    
 });
-  
