1500c9a525b833095d9270ef5789774db99bd2ae
ITkah <ilatkacenko132@gmail.com>
2020-08-28 18:09:42 +0300

    blog

--- js/common.js
+++ js/common.js
@@ -4,6 +4,10 @@ $(document).ready(function() {
         $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
     });
 
+    $('.name').on('input', function() {
+        $(this).val($(this).val().replace(/[0-9,+-_/';:}{}?.<>,()*&^%$#@!]/, ''))
+    });
+
     $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
         $(this).addClass('active').siblings().removeClass('active').closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
@@ -30,4 +34,22 @@ $(document).ready(function() {
         $(this).toggleClass("active-elem").closest('li').find('ul').eq(0).slideToggle(); 
     });
     
+    $('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+
 });
+
+
